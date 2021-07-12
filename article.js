823e4e63ec5c976b7423de3b93b522df58662c4c
nickdelots <66481391+nickdelots@users.noreply.github.com>
2021-07-12 10:21:33 -0400

    Update article.js

--- article.js
+++ article.js
@@ -2,13 +2,8 @@
     {
       "@context": "https://schema.org",
       "@type": "NewsArticle",
-      "headline": "Article headline",
-      "image": [
-        "https://example.com/photos/1x1/photo.jpg",
-        "https://example.com/photos/4x3/photo.jpg",
-        "https://example.com/photos/16x9/photo.jpg"
-       ],
-      "datePublished": "2015-02-05T08:00:00+08:00",
-      "dateModified": "2015-02-05T09:20:00+08:00"
+      "headline": "Insert title here",
+      "image": "https://insert image address",   
+      "datePublished": "2021-07-07T13:42:33+0000"
     }
     </script>
