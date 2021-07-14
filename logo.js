dda769d54fcb39d65e6980270f127da29a3a0f93
nickdelots <66481391+nickdelots@users.noreply.github.com>
2021-07-14 09:49:19 -0400

    Create logo.js

---
+++ logo.js
@@ -0,0 +1,8 @@
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "url": "http://www.example.com",
+      "logo": "http://www.example.com/images/logo.png"
+    }
+    </script>
