a76b415a41d32b4bf38f3dc8e88e2e8ce6fb80b3
nickdelots <66481391+nickdelots@users.noreply.github.com>
2021-07-22 14:02:06 -0400

    Create local-business.js

---
+++ local-business.js
@@ -0,0 +1,52 @@
+<script type="application/ld+json">
+   {
+     "@context": "https://schema.org",
+     "@type": "LocalBusiness",
+     "image": [
+       "https://monsieurfissure.com/wp-content/uploads/2020/09/logo01.jpg",
+       "https://monsieurfissure.com/wp-content/uploads/2020/09/105707330_312620960137767_8790359306612106328_n.jpg"
+      ],
+     "@id": "https://monsieurfissure.com/",
+     "name": "Monsieur Fissure",
+     "address": {
+       "@type": "PostalAddress",
+       "streetAddress": "299 Chemin Industriel #1",
+       "addressLocality": "Gatineau",
+       "addressRegion": "QC",
+       "postalCode": "J8R 0C6",
+       "addressCountry": "CA"
+     },
+     "review": {
+       "@type": "Review",
+       "reviewRating": {
+         "@type": "Rating",
+         "ratingValue": "5",
+         "bestRating": "5"
+       },
+        "author": {
+          "@type": "Person",
+          "name": "Marc Jean-Pierre"
+        }
+     },
+     "geo": {
+       "@type": "GeoCoordinates",
+       "latitude": 45.514832,
+       "longitude": -75.570419
+     },
+     "url": "https://monsieurfissure.com/",
+     "telephone": "+18196436403",
+     "openingHoursSpecification": [
+       {
+         "@type": "OpeningHoursSpecification",
+         "dayOfWeek": [
+           "Monday",
+           "Tuesday",
+           "Wednesday",
+           "Thursday",
+           "Friday"
+         ],
+         "opens": "8:00",
+         "closes": "16:00"
+       }]
+   }
+   </script>
