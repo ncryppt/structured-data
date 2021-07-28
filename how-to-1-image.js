270f803e385607f248bdba9ffc91109c12717591
nickdelots <66481391+nickdelots@users.noreply.github.com>
2021-07-28 09:01:10 -0400

    Create how-to-1-image.js

---
+++ how-to-1-image.js
@@ -0,0 +1,44 @@
+<script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "HowTo",
+      "name": "How to execute a Will",
+      "description": "The estate executor will be responsible for carrying out the will’s instructions and overseeing the distribution of an estate’s assets. Here's what you need to know.",
+      "image": {
+        "@type": "ImageObject",
+        "url": "https://ce-prod-ca-central-1-website-assets.s3.ca-central-1.amazonaws.com/blog/Couple-Executing-Will.webp",
+        "height": "375",
+        "width": "249"
+       },
+   "step": [
+        {
+          "@type": "HowToStep",
+          "name": "Submitting the will to probate court",
+          "url": "https://www.clearestate.com/blog/how-to-execute-a-will",
+          "itemListElement": [{
+            "@type": "HowToTip",
+            "text": "The will can only be submitted to the court of the province or territory in which the estate holder passed away."
+          }, {
+            "@type": "HowToDirection",
+            "text": "The first step an executor needs to take is submitting the will to the appropriate probate court"
+        }]
+       }, {
+          "@type": "HowToStep",
+          "name": "Authenticating the will",
+          "url": "https://www.clearestate.com/blog/how-to-execute-a-will",
+          "itemListElement": [{
+            "@type": "HowToDirection",
+            "text": "Once the will is submitted to the appropriate probate court, the court will authenticate the will. Basically, this just means that the court will bestow its stamp of approval upon the will, including the naming of the executor, and give an official declaration that the executor is authorized to settle the estate. Although this step may sound kind of redundant, many banks and other financial institutions require a court-issued authorization before they’ll allow an executor to access the deceased estate holder’s assets."
+           }]
+          }, {
+          "@type": "HowToStep",
+          "name": "Being officially named the estate executor",
+          "url": "https://www.clearestate.com/blog/how-to-execute-a-will",
+          "itemListElement": [{
+            "@type": "HowToDirection",
+            "text": "Once the estate executor has been officially authorized by the probate court, it’s time to start settling the estate. This is where the real work for the executor begins. They’ll have to start compiling a list of the estate’s assets, appraise the value of the estate, stay in touch with beneficiaries and keep them informed of the probate process, and much more."
+        }]
+       }
+     ]
+      }
+    </script>
